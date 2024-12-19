e7146287ce3d05904035a78bb29f140eb0b400a1
manel-A-tech <manelameziane215@gmail.com>
2024-12-19 23:00:21 +0100

    updated all the functions + statred on part02

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+// function to display the matrix
+void DisplayMatrix(char matrix[][100], int rows) {
+    for (int i = 0; i < rows; i++) {
+        printf("{%s}\n", matrix[i]);
+    }
+    printf("\n");
+}
 
 // display function
 void Display (int arr [] , int sizeofArr){
@@ -8,12 +15,14 @@ void Display (int arr [] , int sizeofArr){
 }
 printf("\n");
 }
+
 // selection sorting
 void SelectionSorting (int arr [] , int sizeofArr){
     int min ;
     int temp ;
     int nbrComp = 0 ;
     int nbrPerm = 0;
+
   for( int i = 0 ; i < sizeofArr -1 ;  i++){
     min =  i ;
     for (int j = i+1 ; j < sizeofArr ; j++){
@@ -26,66 +35,109 @@ void SelectionSorting (int arr [] , int sizeofArr){
     arr[i] = arr[min];
     arr[min] = temp ;
     nbrPerm++ ;
+    printf("Permutation %d: ", i + 1);
     Display(arr , sizeofArr);
   }
   printf("Number of comparisons : %d \n" , nbrComp);
-  printf("numver of permutations : %d \n" , nbrPerm);
+  printf("Number of permutations : %d \n" , nbrPerm);
 }
 
 // bubble sorting
-void BubbleSorting (int arr [] , int sizeofArr){
-    int swap;
-for (int i = 0 ; i < sizeofArr - 1 ; i++ ){
-  for (int j = 0 ; j < sizeofArr - 1 -i ; j++){
-    if(arr[j] > arr[j+1]){
-     swap = arr[j];
-     arr[j] = arr [j+1];
-     arr[j+1] = swap ;
-    //dispaly every progress
-    printf("After swapping %d and %d: ", arr[j + 1], arr[j]);
-    Display(arr , sizeofArr);
+void bubbleSort(int arr[], int sizeofArr) {
+    int i;
+    int j;
+    int temp;
+    int nbrComp = 0 ;
+    int nbrPerm = 0;
+    for (i = 0; i < sizeofArr - 1; i++) {
+        for (j = 0; j < sizeofArr - i - 1; j++) {
+            nbrComp++;
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                nbrPerm++;
+                printf("Permutation %d: ", nbrPerm);
+                Display(arr , sizeofArr);
+            }
+        }
+    }
+    printf("\nNumber of comparisons: %d\n", nbrComp);
+    printf("Number of permutations: %d\n", nbrPerm);
+}
+// bubble sort function for a matrix of characters
+void BubbleSortMatrix(char matrix[][100], int rows) {
+    char temp;
+    int nbrComp = 0 ;
+    int nbrPerm = 0 ;
 
-}}}}
+    for (int i = 0; i < rows; i++) {
+        int sizeofLine = strlen(matrix[i]);
+        // Bubble sort for the current row
+        for (int j = 0; j < sizeofLine - 1; j++) {
+            for (int k = 0; k < sizeofLine - j - 1; k++) {
+                if (matrix[i][k] > matrix[i][k + 1]) {
+                    // Swap the characters
+                    temp = matrix[i][k];
+                    matrix[i][k] = matrix[i][k + 1];
+                    matrix[i][k + 1] = temp;
+                    nbrPerm++;
+                printf("swap number %d\n" , nbrPerm);
+                DisplayMatrix(matrix, rows);
+                }
+                nbrComp++;
+            }
+        }
+    }
+    printf("nombre de comparaison : %d \n" , nbrComp);
+    printf("nombre de permutations : %d\n", nbrPerm);
+}
+
 
 // the function merge
-void Merge ( int arr[] , int low , int mid , int high){
+ // global variables fot comp & perm counters
+int nbrComp = 0;
+int nbrPerm = 0 ;
+void Merge ( int arr[] , int low , int mid , int high ){
     int temp [high - low + 1];
     int left = low ;
     int right = mid + 1 ;
     int j = 0;
-
     while(left <= mid && right <= high){
         if (arr[left] <= arr[right]){
             temp[j] = arr[left];
             left ++ ;
             j++ ;
+            nbrPerm++;
         } else {
         temp[j] = arr[right];
         right ++ ;
         j++;
+        nbrPerm++;
         }
+        nbrComp++;
     }
     while (left <= mid ){
         temp[j] = arr[left];
         left ++;
         j++;
+        nbrPerm++;
     }
     while (right <= high){
         temp[j] = arr[right];
         right ++ ;
         j++;
+        nbrPerm++;
     }
     //arr = temp ;
         for (int k = 0; k < high - low +1; k++) {
         arr[low + k] = temp[k];
     }
-    printf("Merging range [%d-%d]: ", low, high);
-    for (int k = low; k <= high; k++) {
-        printf("%d ", arr[k]);
-    }
-    printf("\n");
-}
 
+    printf("merged array in [%d , %d] ", left , right);
+    Display(arr, right +1 );
+}
+//merge sort function
 void MergeSort (int arr [] , int low , int high){
     if (low < high){
         int mid = (low + high) / 2 ;
@@ -93,7 +145,6 @@ void MergeSort (int arr [] , int low , int high){
         MergeSort(arr , mid +1 , high);
         Merge( arr , low , mid , high);
     }
-
 }
 
 int main()
@@ -101,23 +152,36 @@ int main()
     int array [] = {64 , 34 , 25 , 12 , 22 , 11 , 90};
     int arraySize = sizeof(array) / sizeof(array[0]);
 
-   printf ("original array \n");
-   Display(array , arraySize);
+    char matrix[][100] = {"gfr","zvc","qwe", "axd"};
+    int rows = sizeof(matrix) / sizeof(matrix[0]);
 
-    printf("each step of the slection sorting process \n");
+/*
+    printf ("original array \n");
+    Display(array , arraySize);
+
+    printf("Selection sorting execution \n");
     SelectionSorting(array , arraySize);
     printf("sorted array using selection sorting \n ");
     Display(array , arraySize);
 
-    BubbleSorting(array , arraySize);
+    printf("Bubble sorting execution \n");
+    bubbleSort(array , arraySize);
     printf("sorted array using bubble sorting \n ");
     Display(array , arraySize);
 
-    MergeSort(array , 0 , arraySize -1);
+    printf("merge sort execution \n");
+    MergeSort(array , 0 , arraySize -1 );
+    printf("comparisions %d \n", nbrComp);
+    printf("permutations %d \n" , nbrPerm);
     printf("sorted array using merge sorting \n");
-    Display(array , arraySize);
+    Display(array , arraySize);*/
 
+    printf("Original Matrix:\n");
+    DisplayMatrix(matrix, rows);
 
+    BubbleSortMatrix(matrix, rows);
+    printf("matrix after bubble sort:\n");
+    DisplayMatrix(matrix, rows);
 
 
     return 0;
